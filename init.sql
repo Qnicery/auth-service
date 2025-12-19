304d5d2691d67f68c2b2c403fdc219daeff3b348
Alexandr Mishin <mishin.a.a3@edu.mirea.ru>
2025-12-20 00:20:11 +0300

    ci/cd

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA IF NOT EXISTS auth;
+
+CREATE TABLE IF NOT EXISTS auth.users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
